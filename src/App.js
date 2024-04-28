dcda1457801c7d310476a85cf68b6bbcd6ba2cbc
Morris Jansen <morris8jansen@hotmail.com>
2024-04-28 21:15:11 +0200

    nogsteeds niet

--- src/App.js
+++ src/App.js
@@ -1,38 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-import Navbar from '../componenten/Navbar.js';
-import Home from './Home.js';
-import OverOns from './OverOns.js';
-import Repertoire from './Repertoire.js';
-import Luisteren from './Luisteren.js';
-import AdminPage from './AdminPage';
-import Login from './Login';
-import Referenties from './Referenties.js';
-import BoekingenContact from './BoekingenContact.js';
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/OverOns" element={<OverOns />} />
-          <Route path="/repertoire" element={<Repertoire />} />
-          <Route path="/luisteren" element={<Luisteren />} />
-          <Route path="/admin" component={AdminPage} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Referenties" element={<Referenties />} />
-          <Route path="/Boekingen/Contact" element={<BoekingenContact />} />
-
-
-
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
