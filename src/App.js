661c0166d5df57d8b4c5fae889f06898b3731e56
Morris Jansen <morris8jansen@hotmail.com>
2024-04-28 19:02:27 +0200

    aub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from '../componenten/Navbar.js';
+import Home from './Home.js';
+import OverOns from './OverOns.js';
+import Repertoire from './Repertoire.js';
+import Luisteren from './Luisteren.js';
+import AdminPage from './AdminPage';
+import Login from './Login';
+import Referenties from './Referenties.js';
+import BoekingenContact from './BoekingenContact.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/OverOns" element={<OverOns />} />
+          <Route path="/repertoire" element={<Repertoire />} />
+          <Route path="/luisteren" element={<Luisteren />} />
+          <Route path="/admin" component={AdminPage} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/Referenties" element={<Referenties />} />
+          <Route path="/Boekingen/Contact" element={<BoekingenContact />} />
+
+
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
