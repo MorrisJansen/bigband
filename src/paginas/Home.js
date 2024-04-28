69ff812b4d5ed9d6e8557273b1906c2cba53298e
Morris Jansen <morris8jansen@hotmail.com>
2024-04-28 17:50:00 +0200

    relatief pad

--- src/paginas/Home.js
+++ src/paginas/Home.js
@@ -35,7 +35,7 @@ function Home() {
       <div className="thuisscherm-inhoud">
         <div className="foto-container">
           <h1 className={`foto-heading text-center w-100`}>Welkom bij BigBand Enterprise</h1>
-          <img src='../public/fotos/homepageSlider.webp' alt='homepageSlider' className='homepageSlider' />
+          <img src='/fotos/homepageSlider.webp' alt='homepageSlider' className='homepageSlider' />
         </div>
       </div>
       <div className="tekst-container">
